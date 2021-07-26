f26c4788d484bd1b884a4f2785fdb04a24328858
JS <jrendon@edu.uag.mx>
2021-07-26 17:19:51 -0500

    Camnbio de versión

--- arteycultura/settings.py
+++ arteycultura/settings.py
@@ -122,10 +122,10 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,"static", "static")
+#STATIC_ROOT = os.path.join(BASE_DIR,"static", "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
